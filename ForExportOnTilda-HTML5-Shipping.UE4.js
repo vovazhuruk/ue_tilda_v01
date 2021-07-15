99d4ac81121854760e15d7e6c7654ade184c4f09
vovazhuruk <81244472+vovazhuruk@users.noreply.github.com>
2021-07-15 14:36:06 +0300

    Update ForExportOnTilda-HTML5-Shipping.UE4.js

--- ForExportOnTilda-HTML5-Shipping.UE4.js
+++ ForExportOnTilda-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/vovazhuruk/ue_tilda_v01/9f534ad27cbb117189cec32c24cc9b2c8303c1e8/UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
@@ -1154,8 +1154,8 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('ForExportOnTilda-HTML5-Shipping.data.js'));
-		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/vovazhuruk/ue_tilda_v01/9f534ad27cbb117189cec32c24cc9b2c8303c1e8/ForExportOnTilda-HTML5-Shipping.data.js'));
+		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/vovazhuruk/ue_tilda_v01/9f534ad27cbb117189cec32c24cc9b2c8303c1e8/Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
 			fetchOrDownloadAndStore(db, Module.locateFile('ForExportOnTilda-HTML5-Shipping.data')).then(function(dataBlob) {
